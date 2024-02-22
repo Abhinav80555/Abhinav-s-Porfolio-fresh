f98cbe5c4218343e2fe0945632a188773d5093d5
Abhinav <abhinavsekar1515@gmail.com>
2024-02-22 10:21:43 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
             </Link>
             <a
               className="group text-white w-fit px-6 py-3 my-2 ml-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-              href="https://drive.google.com/file/d/1UThIMuEW1TDBE9ylVNKaW7YbOp7qJPA0/view?usp=sharing"
+              href="https://drive.google.com/file/d/18wgz5UX1TqdvrViDsN1i90Otjfnm2dSz/view?usp=sharing"
               target="_blank"
               rel="noreferrer"
             >
